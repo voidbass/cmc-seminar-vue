8a8f852eceded56968ba257828f0c33da219417d
Khoi <khoi.hm@neo-lab.vn>
2022-12-08 23:47:26 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,24 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="display-flex">
+      <ComponentVue2 />
+      <component-vue-3 />
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ComponentVue2 from './components/ComponentVue2.vue'
+import ComponentVue3 from './components/ComponentVue3.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ComponentVue2,
+    ComponentVue3,
   }
 }
 </script>
@@ -24,5 +32,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.display-flex {
+  display: flex;
+  justify-content: space-evenly;
 }
 </style>
---
+++ src/components/ComponentVue2.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="vue2">
+    <div>This is count: {{ count }}</div>
+    <div>This is computedCount: {{ computedCount }}</div>
+    <button v-on:click="increaseCount">Button 1</button>
+    <button @click="count--">Button 2</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponentVue2",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  computed: {
+    computedCount() {
+      return this.count * 20;
+    },
+  },
+  watch: {
+    count: {
+      handler(value, oldValue) {
+        console.log("new Value", value);
+        console.log("old Value", oldValue);
+      },
+    },
+  },
+  methods: {
+    increaseCount() {
+      this.count = this.count + 1;
+    },
+    decreaseCount() {
+      this.count = this.count - 1;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
