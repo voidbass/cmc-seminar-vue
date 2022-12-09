ae9a13c4b57acc72706dc912bcac554df646e37e
Khoi Huynh <hmkhoi@cmcglobal.vn>
2022-12-09 13:57:01 +0700

    add new

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
     <div class="display-flex">
       <ComponentVue2 />
       <component-vue-3 />
+      <div>
+        Emit outside: {{ isFalseEmit }}
+        <TestEmit @handleEmit="handleEmit" :isFalseEmit="isFalseEmit" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import ComponentVue2 from './components/ComponentVue2.vue'
-import ComponentVue3 from './components/ComponentVue3.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import ComponentVue2 from "./components/ComponentVue2.vue";
+import ComponentVue3 from "./components/ComponentVue3.vue";
+import TestEmit from "./components/TestEmit.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
     ComponentVue2,
     ComponentVue3,
-  }
-}
+    TestEmit,
+  },
+  data() {
+    return {
+      isFalseEmit: false,
+    };
+  },
+  methods: {
+    handleEmit(emitProps) {
+      this.isFalseEmit = emitProps;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TestEmit.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    Test Emit props: {{ isFalseEmit }}
+    <div>Test Emit inside: {{ isFalseInside }}</div>
+    <button @click="changeBoolean">click to change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestEmit",
+  props: {
+    isFalseEmit: {
+      type: String,
+    },
+    handleEmit: {
+      type: Function,
+    },
+  },
+  data() {
+    return {
+      isFalseInside: false,
+    };
+  },
+  watch: {
+    isFalseEmit(val, oldVal) {
+      console.log("change",val, oldVal)
+    },
+  },
+  methods: {
+    changeBoolean(e) {
+      this.isFalseInside = !this.isFalseInside;
+      this.$emit("handleEmit", !e.target.value);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped></style>
